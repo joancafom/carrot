bfe684e4ae44f1d45e6ef1dc203844d3f7a10862
Jose Antonio <joscarfom@alum.us.es>
2019-04-20 22:27:27 +0200

    <feat> Hough Transform
    Also, threshold was modified

--- laneDetection/preprocessing.py
+++ laneDetection/preprocessing.py
@@ -1,6 +1,7 @@
 import cv2
+import numpy as np
 
-image = cv2.imread("./base_imagenes/bd_1.jpg")
+image = cv2.imread("./base_imagenes/bd_10.jpg")
 (h, w, d) = image.shape
 print("alto: {}, ancho: {}, profundidad: {}".format(h, w, d))
 cv2.imshow("Imagen", image)
@@ -21,7 +22,7 @@ gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
 cv2.imshow("Gray", gray)
 cv2.waitKey(0)
 
-thresh = cv2.threshold(gray, 165, 255, cv2.THRESH_BINARY)[1]
+thresh = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY)[1]
 cv2.imshow("Thresh", thresh)
 cv2.waitKey(0)
 
@@ -31,4 +32,12 @@ cv2.waitKey(0)
 
 edged2 = cv2.Canny(thresh, 50, 150)
 cv2.imshow("Edged2", edged2)
+cv2.waitKey(0)
+
+lines = cv2.HoughLinesP(edged2, 1, np.pi/100, 40, minLineLength=50, maxLineGap=10)
+
+for line in lines:
+    x1, y1, x2, y2 = line[0]
+    cv2.line(roi, (x1, y1), (x2, y2), (255, 0, 0), 3)
+cv2.imshow("Hough", roi)
 cv2.waitKey(0)
