<feat, fix> New rotation and Lane Detection
Solved a bug that caused the received image to present
a 2px-pixel wide black line on the left side. New lane detection
based on the three case principle (RI-A/B/C)

lanes/RoadImage.py
@@ -1,9 +1,31 @@
 import cv2
 import numpy as np
 
+from debugtools import log, LOG_LEVEL_ERROR, LOG_LEVEL_WARNING
+
 """
-Class that process & analyses the raw image
-received from the Android device wirelessly.
+Class that process & analyses a road image in 
+order to identify the lanes that appear in it
+and to compute the offset distance that separates
+the driver to the center of the lane.
+
+The image of the road can represent any of the
+following scenarios, so the class is well 
+prepared to fully support them:
+
+    * Image with two lanes [RI-A]
+    * Image with one one lane (either left or right) [RI-B]
+    * An image without any lanes [RI-C]
+
+
+The following assumptions on the image are made:
+
+    * Lane marks are black
+    * There are no other black marks in the image
+    * The environment is light compared to the lanes,
+    it does not need to be regular or unicolored.
+    * There is enough contrast between the lane marks
+    and the environment
 
 """
 
@@ -26,15 +48,14 @@ class RoadImage:
 
         self.bisection_bottom = None
 
-        self.analyse()
-
     ###### GETTERS #####
 
     # Cascade Getters that allow access to
     # every single image in our pipeline
 
     def get_image(self):
-        return self.image
+        # Prevent modifications in the view by making a copy
+        return self.image.copy()
 
     def get_grayscale(self):
         return cv2.cvtColor(self.get_image(), cv2.COLOR_BGR2GRAY)
@@ -70,7 +91,7 @@ class RoadImage:
                 x1, y1, x2, y2 = line[0]
                 cv2.line(hough_image, (x1, y1), (x2, y2), (255, 0, 0), 3)
         else:
-            print("WARNING! No HoughLines were detected!")
+            log("[RI-C] No candidate Hough Lines were found", level=LOG_LEVEL_ERROR)
 
         return hough_image
 
@@ -91,41 +112,116 @@ class RoadImage:
     def analyse(self, hough_rho_resolution=1, hough_thetha_resolution=np.pi/100, hough_threshold_votes=30, hough_minLineLength=5, 
     hough_maxLineGap=10):
 
-        print("Watch out for image size! Less pixels means less votes")
+        log("Watch out for image size! Less pixels means Less votes")
 
-        # STEP 1: Extract Hough lines
+        # ---------- STEP 1: Extract Hough lines ----------
+        #  
+        ## In this step we identify the possible lines that appear in our image.
+        ## In order to be considered as such, the algorithm goes through a voting process
+        ## and each of the candidates must get enough votes to be considered a line.
+
         hough_lines = cv2.HoughLinesP(self.get_edged(), hough_rho_resolution, hough_thetha_resolution, 
             hough_threshold_votes, minLineLength=hough_minLineLength, maxLineGap=hough_maxLineGap)
 
+        ## If no candidate lines were identified, we are in [RI-C] and we cannot successfully 
+        ## estimate the distance, we don't have enough reference.
         if hough_lines is None:
-            print("WARNING! No HoughLines were detected!")
+            
+            log("[RI-C] No candidate Hough Lines were found", level=LOG_LEVEL_ERROR)
             return (None, None, None, None, None)
+        
+        ##
+        #
+        # -------------------------------------------------
 
-        # STEP 2: Figure out where the lanes are 
+        # ---------- STEP 2: Figure out where lanes are ----------
+        #  
+        ## We now go through every candidate line and keep only the two of them which
+        ## correspond to the lane markings (one for each side)
+
         ((left_point, left_point_partner), (right_point, right_point_partner)) = self._compute_nearest_lanes(hough_lines)
 
-        # STEP 3: Extend lanes lines so they take up the whole image
+        ## The two lane markings must be separated at least 100px of each other.
+        ## If not, we consider that one of the markings is wrong and drop one of them
+        ## in order to apply the reflection method (to estimate the distance) 
+        lanes_dist = self.compute_distance_points(left_point, right_point)
+        lanes_dist2 = self.compute_distance_points(left_point, right_point_partner)
 
+        if lanes_dist and lanes_dist < MIN_INTERLANE_PX or lanes_dist2 and lanes_dist2 < MIN_INTERLANE_PX:
+            right_point = None
+            right_point_partner = None
 
-        # Compute the parameters that characterize both lines (the slope and the independent term)
+        ##
+        #
+        # -------------------------------------------------
+
+        # ---------- STEP 3: Extend lanes lines so they take up the whole image ----------
+        #
+        ## We need to extend the lines in order to ensure they pass by the x axis,
+        ## which is the bottom-most point and thus it is less altered by perspective
+        ## so that we can correctly compute the distance.
+        ##
+        ## Up to this point, we need to figure out what lanes where identified correctly
+        ## (left, right or both) (we have cleared out the possibility of both of them being
+        ## incorrectly identified in the previous assertion)
+
         if left_point is not None:
+
+            # Left lane was correctly identified: Either [RI-A] or [RI-B]
+
+            # Get the parameters the characterize left lane mark before we proceed
             left_m = self.compute_slope(left_point[0], left_point[1], left_point_partner[0], left_point_partner[1])
             left_n = self.compute_independent(left_point[0], left_point[1], left_point_partner[0], left_point_partner[1])
-        else:
-            # Left lane does not appear in the photo, we replace it by means of using an approximation
-            print("Left lane was not identified correctly...")
-            left_m = self.compute_slope(np.float64(0), np.float64(1), np.float64(0), np.float64(132))
-            left_n = self.compute_independent(np.float64(0), np.float64(1), np.float64(0), np.float64(132))
+
+            if right_point is None:
+
+                # We are in case [RI-B], only left lane was correctly identified
+                log("[RI-B] - Only Left lane was correctly identified")
+
+                # We reflect the left lane to obtain the right one and separate both 
+                # lines in the picture by the width of a theoretical correct lane.
+                right_n = self.compute_reflected_independent(left_m, left_point, INTERLANE_PX)
+                right_m = -left_m
         
         if right_point is not None:
+
+            # Right lane was correctly identified: Either [RI-A] or [RI-B]
+
+            # Get the parameters the characterize right lane mark before we proceed
             right_m = self.compute_slope(right_point[0], right_point[1], right_point_partner[0], right_point_partner[1])
             right_n = self.compute_independent(right_point[0], right_point[1], right_point_partner[0], right_point_partner[1])
-        else:
-            # Right lane does not appear in the photo, we replace it by means of using an approximation
-            print("Right lane was not identified correctly...")
-            right_m = self.compute_slope(np.float64(199), np.float64(1), np.float64(199), np.float64(132))
-            right_n = self.compute_independent(np.float64(199), np.float64(1), np.float64(199), np.float64(132))
 
+            if left_point is None:
+
+                # We are in case [RI-B], only right lane was correctly identified
+                log("[RI-B] - Only Right lane was correctly identified")
+
+                # We reflect the right lane to obtain the left one and separate both 
+                # lines in the picture by the width of a theoretical correct lane.
+                left_n = self.compute_reflected_independent(right_m, right_point, INTERLANE_PX)
+                left_m = -right_m
+
+        ## Now, we have to ensure that both lines intersect. Two lines do not intersect
+        ## if either they are parallel (same slope) or the same (same slope and independent)
+
+        if left_m == right_m:
+
+            if left_n == right_n:
+
+                # Same lines, infinite intersection points, so we just reflect one
+                # of them as if we had only identified that one.
+
+                left_n = self.compute_reflected_independent(right_m, right_point, INTERLANE_PX)
+                left_m = -right_m
+            
+            else:
+
+                # Parallel lines: We change a the slope of one of them just a little
+                # bit to transform it into intersecting
+
+                left_m += 0.01
+
+        ## We now obtain the drawing points (upper-most and bottom-most) of both lanes
 
         # Left Lane Line's Points: The upper-most and bottom-most points corresponding 
         # to the line, in order to draw it
@@ -135,30 +231,43 @@ class RoadImage:
         # to the line, in order to draw it
         self.right_lane_top, self.right_lane_bottom = self.compute_line_drawing_points(right_m, right_n)
 
-        # STEP 4: Compute the intersection of both lines
+        ##
+        #
+        # -------------------------------------------------
 
-        # We need the intersection point between the two lines
-        # We can do so, by solving a simple equation system with two unknowns:
-        #----------------
-        # | y = mx + n
-        # | y = m'x + n' 
-        #----------------
-        # x = (n - n') / (m' - m)
+        # ---------- STEP 4: Compute the intersection of both lines ---------- 
+        #
+        ## We need the intersection point between the two lines
+        ## We can do so, by solving a simple equation system with two unknowns:
+        ## ----------------
+        ## | y = mx + n
+        ## | y = m'x + n' 
+        ## ----------------
+        ## x = (n - n') / (m' - m)
+
         intersection_x = (left_n - right_n) / (right_m - left_m)
         intersection_y = left_m * intersection_x + left_n
         intersection = (intersection_x, intersection_y)
-        
-        # STEP 5: Compute the position of the car using the bisection of the angle
 
-        # ----- INTERCENTER ----
+        ##
+        #
+        # -------------------------------------------------
+        
+        # ---------- STEP 5: Compute the position of the car using the bisection of the angle ----------
+        #
+        ## INTERCENTER
         # We compute the intercenter of a triangle formed by the intersection point and two 
-        # arbitrary points of each line
+        # arbitrary points of each line, and after that, we get the middle point of the lane.
         incenter = self.compute_incenter(intersection, self.left_lane_bottom, self.right_lane_bottom)
 
         m = self.compute_slope(incenter[0], incenter[1], intersection[0], intersection[1])
         n = self.compute_independent(incenter[0], incenter[1], intersection[0], intersection[1])
 
         _, self.bisection_bottom = self.compute_line_drawing_points(m, n)
+
+        ##
+        #
+        # -------------------------------------------------
 
         # Points are returned from left lane in a clockwise direction 
         return (self.left_lane_bottom, intersection, self.right_lane_bottom, self.bisection_bottom, self.bottom_center)
@@ -233,7 +342,23 @@ class RoadImage:
         either x2 or x1 to keep it bounded.
     '''
     def compute_slope(self, x1, y1, x2, y2):
-        return (y2-y1)/(x2-x1) if x2 != x1 else (y2-y1)
+        
+        # We must prevent the slope
+        # from being 0 or inf
+
+        if y1 == y2:
+            
+            # Slope that produces an almost
+            # flat line without being zero
+            return 0.001
+        
+        if x1 == x2:
+
+            # Slope that produces an almost
+            # straight line without being inf
+            return 999
+            
+        return (y2-y1)/(x2-x1)
 
     '''
         Computes the independent term of a line that two given 
@@ -298,6 +423,17 @@ class RoadImage:
 
 
         return res
+
+    '''
+        Computes the independent coefficient of a line with slope the inverse
+        to the original, separated to one point by a given distance.
+        
+        (x, y) & (x + dist, y)
+        y = mx+n <-> n' = y + m'(x + dist)
+
+    '''
+    def compute_reflected_independent(self, original_slope, point, px_distance):
+        return point[1] + original_slope*(point[0] + px_distance)
     
     def int_point(self, point):
         return (int(point[0]), int(point[1]))

tcpServer.py
@@ -79,36 +79,15 @@ def tcp_server():
                     opencvImage = np.array(pimg)
 
                     # Fix width to 200px
-                    (h, w, d) = opencvImage.shape
-                    r = 200.0 / float(h)
                     dim = (268, 200)
                     resizedI = cv2.resize(opencvImage, dim)
 
                     # Rotate the image
-                    (h2, w2) = resizedI.shape[:2]
-                    (cX, cY) = (w2 // 2, h2 // 2)
-
-                    # grab the rotation matrix (applying the negative of the
-                    # angle to rotate clockwise), then grab the sine and cosine
-                    # (i.e., the rotation components of the matrix)
-                    M = cv2.getRotationMatrix2D((cX, cY), -90, 1.0)
-                    cos = np.abs(M[0, 0])
-                    sin = np.abs(M[0, 1])
-
-                    # compute the new bounding dimensions of the image
-                    nW = int((h2 * sin) + (w2 * cos))
-                    nH = int((h2 * cos) + (w2 * sin))
-
-                    # adjust the rotation matrix to take into account translation
-                    M[0, 2] += (nW / 2) - cX
-                    M[1, 2] += (nH / 2) - cY
-
-                    # perform the actual rotation and return the image
-                    rotated90 = cv2.warpAffine(resizedI, M, (nW, nH))
+                    rotated90 = cv2.rotate(resizedI, rotateCode=0)
                     #cv2.imshow("rotated", rotated90)
 
                     # Crop image so its witdh is 200px and its height is 134px
-                    roi = rotated90[nH//2:(nH//2)+134, 0:200]
+                    roi = rotated90[dim[0]//2:(dim[0]//2)+134, 0:200]
                     #cv2.imshow("ROI", roi)
                     #cv2.waitKey(1)
 